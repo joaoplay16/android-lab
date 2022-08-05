4f7fb8659448f4a8f7c8541c605e03879b372aed
João Pedro <joaoplay16@gmail.com>
2022-08-05 19:41:20 -0300

    fragment_b using shared view model

--- app/src/main/java/com/playlab/androidlab/FragmentB.kt
+++ app/src/main/java/com/playlab/androidlab/FragmentB.kt
@@ -5,15 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.playlab.androidlab.databinding.FragmentABinding
 import com.playlab.androidlab.databinding.FragmentBBinding
+import com.playlab.androidlab.viewmodel.SharedViewModel
 
 class FragmentB : Fragment() {
 
     private val args by navArgs<FragmentBArgs>()
+    private val sharedViewModel: SharedViewModel by activityViewModels()
 
     lateinit var binding: FragmentBBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,7 +27,23 @@ class FragmentB : Fragment() {
         // Inflate the layout for this fragment
         binding =  FragmentBBinding.inflate(inflater, container, false)
 
-        binding.textView.text = "FirstName ${args.currentUser.firstName} LastName ${args.currentUser.lastName}"
+        sharedViewModel.person.observe(viewLifecycleOwner){
+            binding.edtFirstName.setText(it.firstName)
+            binding.edtLastName.setText(it.lastName)
+        }
+
+        binding.sendBtn.setOnClickListener {
+            val firstName = binding.edtFirstName.text.toString()
+            val lastName = binding.edtLastName.text.toString()
+
+//            val user = User(firstName, lastName)
+//
+//            val action = FragmentADirections.actionFragmentAToFragmentB(user)
+//            findNavController().navigate(action)
+
+            sharedViewModel.setPersonName(firstName, lastName)
+            findNavController().navigate(R.id.action_fragmentB_to_fragmentA)
+        }
 
         return binding.root
     }
